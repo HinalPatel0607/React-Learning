68ac4666a48b3aa1bbf226ee2f02351e837ede87
Hinal <patel.hinal@outlook.com>
2024-07-17 04:27:53 +0530

    Introduction to JSX

--- React/src/components/Hello.jsx
+++ React/src/components/Hello.jsx
@@ -1,5 +1,21 @@
+const name = "Rob";
+function displayMessage() {
+  return "Wow!!!";
+}
 function Hello() {
-  return <div><h1>Hello from a component</h1></div>;
+  return (
+    <div>
+      <h1>
+        Hello from a component
+        <br />
+        {name}
+        <br />
+        {10 + 20}
+        <br />
+        {displayMessage()}
+      </h1>
+    </div>
+  );
 }
 
 export default Hello;
